c348003bcf90e02c874a1f34430a67045ddff5e2
TriTonyyy <quangtri987@gmail.com>
2024-11-26 13:20:27 +0700

    hmm

--- src/Pages/HomePage/homePage.js
+++ src/Pages/HomePage/homePage.js
@@ -12,8 +12,8 @@ export function HomePage() {
 export function HomePageBody() {
   return (
     <div className="space-y-5">
-      <HomePageCollection type="Bàn phím" />
       <HomePageCollection type="Chuột" />
+      <HomePageCollection type="Bàn phím" />
       <HomePageCollection type="Màn hình" />
       <HomePageCollection type="Tai nghe" />
     </div>
