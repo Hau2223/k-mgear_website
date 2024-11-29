22463df812de6fea9c15c9d1f283f4ebfd440f25
HuynhTonyy <leongirl2013@gmail.com>
2024-11-30 00:09:30 +0700

    Tan  complete

--- src/Pages/ProductDetailPage/productDetailPage.js
+++ src/Pages/ProductDetailPage/productDetailPage.js
@@ -37,9 +37,8 @@ const { id } = useParams();
 }
 
 export function ProductDetailPageBody() {
-    const [productData, setProductData] = useState(null);  // Default to null
+    const [productData, setProductData] = useState(null);
     const { id } = useParams();
-
     const userID = localStorage.getItem("userID");
 
 
@@ -61,28 +60,19 @@ export function ProductDetailPageBody() {
     }, [id]));
 
     const handleAddToCart = async () => {
-        // localStorage.removeItem("productIDs")
-        if (userID == null) {
-            const newProductID = {
-                idProduct: id,
-            }
-            const savedData = JSON.parse(localStorage.getItem("productIDs"));
-
-            if (!savedData) {
-                localStorage.setItem("productIDs", JSON.stringify([newProductID]))
+        if (!userID) {
+            const savedProductIDs = JSON.parse(localStorage.getItem("productIDs"));
+            if (!savedProductIDs) {
+                localStorage.setItem("productIDs", JSON.stringify([id]))
+                alert("Thêm thành công!");
+                return
+            }   
+            if(!savedProductIDs.find(data=> data === id)){
+                localStorage.setItem("productIDs", JSON.stringify([...savedProductIDs, id]));
+                alert("Thêm thành công!");
                 return
             }
-            savedData.map(async (data, index) => {
-                if (data.idProduct === newProductID.idProduct) {
-                    await localStorage.setItem("productIDs", JSON.stringify(savedData));
-                    return
-                }
-                if (index === savedData.length - 1) {
-                    localStorage.setItem("productIDs", JSON.stringify([...savedData, newProductID]));
-                    return
-                }
-            })
-
+            alert("Sản phẩm đã có trong giỏ hàng")
         } else {
             const newCart = {
                 idUser: userID,
@@ -93,7 +83,6 @@ export function ProductDetailPageBody() {
             const response = await createCart(newCart)
             alert(response.message);
         }
-
     };
 
     const formatPrice = (price) => {
@@ -107,66 +96,62 @@ export function ProductDetailPageBody() {
 
     return (
         <div className="flex flex-col md:flex-row items-center p-6 max-w-5xl mx-auto my-10 border rounded-lg shadow-lg bg-white">
-            {/* Product Image */}
             <div className="flex-[5] flex justify-center items-center mb-6 md:mb-0 md:mr-8">
-                <div className="w-full h-72 overflow-hidden rounded-lg">
-                    <img
-                        src={productData?.imageUrl || "/path/to/fallback-image.jpg"}
-                        alt={productData?.name}
-                        className="w-full h-full object-contain transform hover:scale-105 transition-transform duration-300"
-                    />
+    <div className="w-full h-72 overflow-hidden rounded-lg">
+        <img
+            src={productData?.imageUrl || "/path/to/fallback-image.jpg"}
+            alt={productData?.name}
+            className="w-full h-full object-contain transform hover:scale-105 transition-transform duration-300"
+        />
+    </div>
+</div>
+<div className="flex-[6] px-4">
+    <h1 className="text-2xl font-bold text-gray-800 mb-4">{productData?.name}</h1>
+    <div className="my-4">
+        {productData?.discount && productData.discount > 0 ? (
+            <>
+                <div className="bg-gradient-to-r from-red-500 to-orange-500 text-white px-4 py-2 inline-block text-sm uppercase font-bold rounded-full mb-2 shadow-lg">
+                    Flash Sale
                 </div>
-            </div>
-
-            {/* Product Details */}
-            <div className="flex-[6] px-4">
-                <h1 className="text-2xl font-bold text-gray-800 mb-4">{productData?.name}</h1>
-
-                <div className="my-4">
-                    {productData?.discount && productData.discount > 0 ? (
-                        <>
-                            {/* Flash Sale Badge */}
-                            <div className="bg-gradient-to-r from-red-500 to-orange-500 text-white px-4 py-2 inline-block text-sm uppercase font-bold rounded-full mb-2 shadow-lg">
-                                Flash Sale
-                            </div>
-
-                            {/* Price Details */}
-                            <div className="flex items-center">
-                                <p className="!text-red-600 text-xl font-semibold mr-2">
-                                    {formatPrice(productData?.price - (productData?.price * (productData?.discount / 100)))}
-                                </p>
-                                <span className="text-gray-500 line-through text-lg mx-2">
-                                    {formatPrice(productData?.price)} VND
-                                </span>
-                                <span className="bg-[#fedee3] text-red-600 text-xs font-bold py-1 px-2 rounded-full ml-2">
-                                    -{productData?.discount}%
-                                </span>
-                            </div>
-                        </>
-                    ) : (
-                        <p className="text-red-600 text-xl font-semibold">
-                            {formatPrice(productData?.price)} VND
-                        </p>
-                    )}
+                {/* Price Details */}
+                <div className="flex items-center">
+                    <p className="!text-red-600 text-xl font-semibold mr-2">
+                        {formatPrice(productData?.price - 
+                            (productData?.price * (productData?.discount / 100)))}
+                    </p>
+                    <span className="text-gray-500 line-through text-lg mx-2">
+                        {formatPrice(productData?.price)} VND
+                    </span>
+                    <span className="bg-[#fedee3] text-red-600 text-xs font-bold py-1 px-2 rounded-full ml-2">
+                        -{productData?.discount}%
+                    </span>
                 </div>
+            </>
+        ) : (
+            <p className="text-red-600 text-xl font-semibold">
+                {formatPrice(productData?.price)} VND
+            </p>
+        )}
+    </div>
 
-                <div className="space-y-2 text-gray-600 mb-8">
-                    <p>
-                        Còn lại:{" "}
-                        <strong>{productData?.quantity}</strong>
-                    </p>
-                    <p>
-                        Hãng: <strong className="text-blue-500">{productData?.brand}</strong>
-                    </p>
-                    <p>
-                        Đã bán:{" "}
-                        <strong className="text-orange-500">{productData?.sold}</strong>
-                    </p>
-                </div>
+    <div className="space-y-2 text-gray-600 mb-8">
+        <p>
+            Còn lại:{" "}
+            <strong>{productData?.quantity}</strong>
+        </p>
+        <p>
+            Hãng: <strong className="text-blue-500">{productData?.brand}</strong>
+        </p>
+        <p>
+            Đã bán:{" "}
+            <strong className="text-orange-500">{productData?.sold}</strong>
+        </p>
+    </div>
                 <div className="flex flex-wrap gap-4">
                     <button
                         className="px-6 py-3 bg-blue-500 text-white rounded-lg text-sm font-semibold hover:bg-blue-600 transition-colors duration-300"
-                        onClick={() => handleAddToCart(productData?._id)}>
+                        onClick={() => 
+                        handleAddToCart(productData?._id)}>
                         Thêm vào giỏ hàng
                     </button>
                 </div>
